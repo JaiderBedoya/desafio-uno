16708f3701c1eb4c8d9fce3abe1d75e7793e71ce
JaiderBedoya <161373759+JaiderBedoya@users.noreply.github.com>
2024-09-14 15:57:07 -0500

    se determinó tipo de onda y frecuencia
    se agregó como calcular la frecuencia y tomar valores en un segundo del arreglo además de un prototipo de determinar tipo de onda

--- codigoFuente.cpp
+++ codigoFuente.cpp
@@ -11,18 +11,29 @@ int valorMin = 1024;
 int amplitud;
 int puntoMedio;
 bool Encendido;
+bool positivo = true;
+int contadorCruce = 0;
+unsigned short int cambios = 0;
+int contador;
 
+bool contando;
 
+unsigned short int frecuencia;
 
+unsigned long tiempoInicial = 0;
 
+const unsigned long segundo = 1000;
 
 bool ciclo;
+
 int idx = 0;
-float tiempoInicial;
+
 unsigned long tiempoTranscurrido;
 
+int* arregloValores = new int[459];
 
-int contadorCiclo = 0;
+unsigned int determinante = 0;
+
 
 void setup()
 {
@@ -44,11 +55,11 @@ void loop()
   
   if(buttonOff == HIGH){
     ciclo = true;
+    Encendido = false;
   }//buttonOff
   
   
   if(Encendido){  
-    
    
     if(val > valorMax){
     	valorMax = val;
@@ -60,36 +71,100 @@ void loop()
     
     amplitud = (valorMax - valorMin)/2;
     
-    puntoMedio = (valorMax + valorMin)/2;
-    
+    //puntoMedio = (valorMax + valorMin)/2;    
      
-    if(ciclo){
-    int* arregloValores = new int[amplitud*2];
-      
-      for(int idx = 0; idx < (amplitud*2); idx++){
-        arregloValores[idx] = analogRead(analogpin);
-        delay(10);
-      }      
+      }//Encendido
+  
+  
+  
+  
+  
+  if(ciclo){
+    
+    if(val== amplitud && !contando){
+      tiempoInicial = millis();
+      contando = true;
+    }
+      if(contando){
+     arregloValores[idx] = analogRead(analogpin);
+    idx++;
+    delay(2);
+       if(millis() - tiempoInicial >= segundo){
       Serial.println("Termino la recoleccion de datos...");
-    delay(8000);
-      for(int i = 0; i < amplitud*2; i++){
-        Serial.print("Indice: ");
-        Serial.println(i);
-        Serial.println(arregloValores[i]);
+      Serial.println(idx);
+    delay(8000);      
+         Serial.println("Imprimiendo datos tomados en un segundo");
+         
+         for(int j = 0; j <= idx;j++){
+         	Serial.println(arregloValores[j]); 
+         }
+         delay(8000);
+         
+         for(int i = 0; i <= idx; i++){
+           
+           if(i < idx){             
+             if(arregloValores[i] == arregloValores[i+1]){
+             	determinante++;
+             }
+
+    
+           if(positivo){
+
+           if(arregloValores[i+1]>0){
+           contadorCruce++;
+           positivo = false;
+           }
+             
+           }  
+           
+           if(!positivo){
+               
+           if(arregloValores[i+1] < 0){
+           contadorCruce++;
+           positivo = true;
+           }
+               
+           }
+             
+           
+           
+           }  
+         }
+         
+         frecuencia = contadorCruce;
+         Serial.println("La frecuencia es: ");
+         if(frecuencia%2 == 0){
+         	Serial.println(frecuencia/2);
+         }
+         else{
+         Serial.println((frecuencia-1)/2);
+         }
+         Serial.println("La amplitud es: ");
+         Serial.println(amplitud);
+         
+         if(determinante > contadorCruce){
+         Serial.println("Onda cuadrada");
+         }
+         else if(determinante == 0){
+         Serial.println("Onda triangular");
+         }
+         else if(determinante != 0 && determinante <= contadorCruce){
+         Serial.println("Onda Sinusoidal");
+         }
+         else{
+         Serial.println("Señal desconocida");
+         }
+         
+         
+         
+      contando = false;
+    	ciclo = false;
+    }
       }
-        ciclo = false;
-      
-    }//ciclo
-    
-    
     
       
-        } 
-    
-    
-    
-    
+
+      }//ciclo
+           
    
-  
-  
 }//loop
